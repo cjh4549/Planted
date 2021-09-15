745aab6468a8b4c6c75e1dd629a9a2047ae7e51a
Jun Choi <cjh4549@gmail.com>
2021-09-14 23:12:55 -0400

    added no scroll event when hamburger menu is activated

--- js/script.js
+++ js/script.js
@@ -2,12 +2,24 @@ const hamburger = document.querySelector('.hamburger');
 const navItems = document.querySelector('.navbar__items');
 const navLink = document.querySelectorAll('.navbar__items li');
 
+const preventScroll = function() {
+    window.scrollTo(0, 0);
+}
+
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle("active");
     navItems.classList.toggle("active");
+
+    window.addEventListener('scroll', preventScroll);
+
+    if (!hamburger.classList.contains("active")) {
+        window.removeEventListener('scroll', preventScroll);
+    }
 })
 
 navLink.forEach(n => n.addEventListener('click', function(){
     hamburger.classList.remove("active");
     navItems.classList.remove("active");
+
+    window.removeEventListener('scroll', preventScroll);
 }))
