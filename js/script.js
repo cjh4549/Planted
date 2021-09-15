4de8bbc23ee1608267e967a9e240ba0aa1fd928a
Jun Choi <cjh4549@gmail.com>
2021-09-14 21:14:32 -0400

    removed hamburger menu when navbar link is clicked

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navItems = document.querySelector('.navbar__items');
+const navLink = document.querySelectorAll('.navbar__items li');
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle("active");
     navItems.classList.toggle("active");
 })
+
+navLink.forEach(n => n.addEventListener('click', function(){
+    hamburger.classList.remove("active");
+    navItems.classList.remove("active");
+}))
