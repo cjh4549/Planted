8d7ccc4cded95371183fc223dd5b29c2cc14130b
Jun Choi <cjh4549@gmail.com>
2021-09-15 23:25:09 -0400

    added innerHTML function to paste comment with user's input and date

--- js/script.js
+++ js/script.js
@@ -28,16 +28,39 @@ const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const comment = document.querySelector('#comment');
 const form = document.querySelector('.form');
+const commentSection = document.querySelector('.blog__article__comment');
 
-const formSubmit = form.addEventListener('submit', function(event){
+form.addEventListener('submit', function(event){
     event.preventDefault();
-    console.log(name.value);
-    console.log(email.value);
-    console.log(comment.value);
+
+    const nameValue = name.value;
+    const emailValue = email.value;
+    const commentValue = comment.value;
+
+    if(nameValue && emailValue && commentValue) {
+        commentPaste(nameValue, printDate);
+    }
 })
 
 const printDate = function(){
     const event = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    console.log(event.toLocaleDateString(undefined, options));
+    return event.toLocaleDateString(undefined, options);
+}
+
+const commentPaste = function(userName, printDate){
+    const newComment = document.createElement('div');
+
+
+    newComment.innerHTML = `
+    <div class="blog__article__comment-img">
+        <img src="../planted-final-assets/profile-image-2.jpg" alt="a photo of user-female">
+    </div>
+    <div class="blog__article__comment-user-text">
+        <h3>${printDate()} by ${userName}</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repellat corporis doloremque aspernatur, est, nisi placeat dolores iste corrupti at earum, quas quia alias cumque eos numquam ullam sed atque.</p>
+    </div>`;
+
+    const blogArticle = document.querySelector('.blog__article__comment');
+    blogArticle.appendChild(newComment);
 }
